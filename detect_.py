83cb2b9d3f87e33ed356e1733c061475dc325e55
gaeSeung <job5284@naver.com>
2020-11-20 22:42:13 +0900

    11202242

--- detect_.py
+++ detect_.py
@@ -3,12 +3,13 @@ import cv2
 from matplotlib import pyplot as plt
 
 face_cascade = cv2.CascadeClassifier('./cascade.xml')
-
-img = cv2.imread('./pos10.jpg')
+print(face_cascade)
+img = cv2.imread('./output1.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 objs = face_cascade.detectMultiScale(gray, 1.3, 5)
+print(objs)
 
 for (x,y,w,h) in objs:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
--- picam_cali.py
+++ picam_cali.py
@@ -4,6 +4,8 @@ from picamera import PiCamera
 import time
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+import threading
 
 # You should replace these 3 lines with the output in calibration step
 DIM=(320, 240)
@@ -22,18 +24,26 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 # allow the camera to warmup
 time.sleep(0.1)
 
+def captured(img):
+    cv2.imwrite(time.strftime('%m%d%H%M%S')+'.jpg', img)
 
+def cascade(img):
+        face_cascade = cv2.CascadeClassifier('./cascade.xml')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        objs = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in objs:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        
 #undistort
 def undistort(img):
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
     return undistorted_img
-    
+
+
 # capture frames from the camera
-
-n=1 #Initialization for capture
-
+checktimeBefore = int(time.strftime('%S'))
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
@@ -43,6 +53,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     #undistort
     undistorted_image = undistort(image)
 
+    #cascade
+    cascade(undistorted_image)
+        
     # show the frame
     cv2.imshow("Frame", undistorted_image)
     key = cv2.waitKey(1) & 0xFF
@@ -50,9 +63,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
 
+    # capture negative images every second
+    switch = 0
+    checktime = int(time.strftime('%S'))
+    if checktime - checktimeBefore >=1 and switch == 1:
+        captured(undistorted_image)
+        checktimeBefore = checktime
+
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
     elif key == ord("\t"):
-        cv2.imwrite('output'+str(n)+'.jpg', undistorted_image)
-        n = n + 1
+        captured(undistorted_image)
+  
